be5e3602413832990bc489e4e6ad51b531ba4c40
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2025-02-25 08:10:45 +0800

    Added HeaderText Component and Different Types of Button Components

---
+++ resources/js/Components/Buttons/MyButton/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="MyButton">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'MyButton',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
