730568a97fb0c509848206aa3ae000633b684354
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2025-02-26 17:19:52 +0800

    removed uneccesary focus class

--- resources/js/Components/Inputs/TextInput.vue
+++ resources/js/Components/Inputs/TextInput.vue
@@ -12,13 +12,13 @@
     const setStatusColor = () => {
         switch (props.status) {
             case "success":
-            return "border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500 focus:border-green-500";
+                return "border-green-500 text-green-900 placeholder-green-700 focus:ring-green-500";
             case "error":
-            return "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500";
+                return "border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500";
             case "warning":
-            return "border-yellow-500 text-yellow-900 placeholder-yellow-700 focus:ring-yellow-500 focus:border-yellow-500";
+                return "border-yellow-500 text-yellow-900 placeholder-yellow-700 focus:ring-yellow-500";
             default:
-            return "border-gray-500 text-gray-900 placeholder-gray-700 focus:ring-gray-500 focus:border-gray-500";
+                return "border-gray-500 text-gray-900 placeholder-gray-700 focus:ring-gray-500";
         }
     };
 </script>
