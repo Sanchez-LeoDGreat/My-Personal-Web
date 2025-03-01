c12af6fe988c93b21ceb19c16c6b8dd0c9c2f598
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2025-03-01 15:55:31 +0800

    renamed the hero image

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,7 +3,7 @@
     import HeaderText from '@/Components/Texts/HeaderText.vue';
     import DarkGlass from '@/Components/Effects/GlassMorphism/DarkGlass.vue';
     import MarginLayout from '@/Layouts/Child/MarginLayout.vue';
-    import selfLogo from '@/assets/img/self-logo.png';
+    import hero from '@/assets/img/hero.png';
     import TypeWriter from '@/Components/Effects/TextEffect/TypeWriter.vue';
 
     const greenText = (txt) => `<span class="text-green-500">${txt}</span>`;
@@ -25,7 +25,7 @@
             <div class="flex flex-col gap-8 m-10 md:gap-0 md:flex-row md:place-items-center">
                 <div class="md:w-[55%]">
                     <HeaderText class="text-3xl">Leonard Paulo Sanchez</HeaderText>
-                    <HeaderText class="">
+                    <HeaderText>
                         <span>I'm </span><TypeWriter class="inline" :strings="mySkills"/>
                     </HeaderText>
                     <div class="flex flex-col gap-2 text-justify">
@@ -43,7 +43,7 @@
                     <button class="px-4 py-2 my-2 font-medium text-black transition-all bg-white rounded-full hover:shadow-green-500 hover:shadow-md active:shadow-blue-500 active:text-blue-500">Download CV</button>
                 </div>
                 <div class="md:w-[45%]">
-                    <img :src="selfLogo" alt="self-logo.png" class="">
+                    <img :src="hero" alt="hero.png">
                 </div>
             </div>
         </DarkGlass>
