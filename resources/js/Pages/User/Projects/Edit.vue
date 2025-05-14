e2a0b57acef3b1ca324dd00012c3381553e86593
Leonard Paulo Sanchez <leonardpaulo.sanchez.3@gmail.com>
2025-05-14 22:55:03 +0800

    Added meta theme color

--- resources/js/Pages/User/Projects/Edit.vue
+++ resources/js/Pages/User/Projects/Edit.vue
@@ -62,7 +62,7 @@
 <template>
     <Head title="Add Project"/>
     <DarkGlass class="min-h-screen p-2">
-        <HeaderText class="mb-1">Edit <span class="text-green-500 ">Project</span></HeaderText>
+        <HeaderText class="mb-1">Edit <span class="text-green-500">Project</span></HeaderText>
         <div class="px-4">
             <div class="my-4">
                 <Link :href="route('projects.manage')">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="theme-color" content="#020617">
     @routes
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     @inertiaHead
